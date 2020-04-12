0da462ea79c37a6b8b56df7f1495d5ed5963a696
Fabian Maume <fabian.maume@laposte.net>
2020-04-12 08:51:10 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -11,6 +11,7 @@ chrome.storage.sync.get('color', function(data) {
   changeColor.setAttribute('value', data.color);
 });
 
+/*
 changeColor.onclick = function (element) {
 
     let satismeter_code = ' (function() { window.satismeter = window.satismeter || function() {(window.satismeter.q = window.satismeter.q || []).push(arguments);};window.satismeter.l = 1 * new Date();var script = document.createElement("script");var parent = document.getElementsByTagName("script")[0].parentNode;script.async = 1;script.src = "https://app.satismeter.com/satismeter.js";parent.appendChild(script);})();'
@@ -34,7 +35,7 @@ displaySurvey.onclick = function (element) {
             { code: satismeter_code2 });
     });
 };
-
+*/
 
 inject_script.onclick = function (element) {
 
